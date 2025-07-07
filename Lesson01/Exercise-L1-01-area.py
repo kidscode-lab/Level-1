a667a7e461fc0c966a68ab8a7b2cf6692ee50497
quincykong <quincykong@gmail.com>
2025-07-06 20:30:26 -0400

    Create Lesson 1 Materials

---
+++ Lesson01/Exercise-L1-01-area.py
@@ -0,0 +1,15 @@
+# L1 - Exercise 02
+#   Develop a program to calculate the area of a triangle
+
+# Initialize
+area = 0
+base = 0
+height = 0
+
+# Process
+print('Calculate area of triangle')
+base = float(input('Base of the triangle:'))
+height = float(input('Height of the triangle:'))
+
+# Output
+print(f'The area is: {base * height / 2}')
